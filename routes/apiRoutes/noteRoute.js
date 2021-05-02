4e51397d82b3ca6660fdc4688cca2f91d1750fbc
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-05-01 18:30:55 -0700

    created POST route for notes

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
+const { createNewNote } = require('../../lib/notes');
+const { notes } = require('../../db/notes');
+
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
 
